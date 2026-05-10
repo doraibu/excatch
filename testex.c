8f8797126598351b4e094ba302cc65b9aac1792e
doraibu <f.santosbrito1@gmail.com>
2026-05-10 14:24:43 -0300

    second impact

--- testex.c
+++ testex.c
@@ -1,22 +1,46 @@
 #define EXCATCH_IMPLEMENTATION
 #include "excatch.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void profunda()
+#define EXC_NULL_PTR 1
+#define EXC_OUT_OF_MEM 2
+#define EXC_DIV_ZERO 3
+#define EXC_IO_ERROR 4
+
+void divn(int a, int b)
 {
-    printf("Entrou na função...\n");
-    exc_throw("main", 404);
+    if (b == 0)
+        exc_throw(__func__, EXC_DIV_ZERO);
+
+    printf("Result: %d\n", a / b);
+}
+
+void readn(const char* path)
+{
+    FILE* f = fopen(path, "r");
+    if (!f)
+        exc_throw(__func__, EXC_IO_ERROR);
+
+    fclose(f);
+}
+
+void procn(int* data, int divisor)
+{
+    if (!data)
+    exc_throw(__func__, EXC_NULL_PTR);
+
+    divn(*data, divisor);
 }
 
 int main()
 {
     try {
-	printf("tentando algo...\n");
-	profunda();
-	printf("Isso nunca vai acontecer");
+	int data[] = {42, 0};
+	procn(data, 0);
+	printf("This will never reach if exception is launched\n");
     } catch {
-	printf("capturando erro!\n");
+	printf("Exception caught! code: %d\n", _err);
     } try_end
-
     return 0;
 }
